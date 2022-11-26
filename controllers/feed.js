349e7ba847b13a8c8452e742f6816f2d902d86fb
KyleRiggen <kyle.riggenbach@gmail.com>
2022-11-25 22:43:48 -0700

    adding dummy data before connecting to database

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,9 +1,14 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [{
+      _id: '1',
       title: 'First Post',
       content: 'This is the first post!??!?!!',
-      imageUrl: 'images/duck.jpg'
+      imageUrl: 'images/duck.jpg',
+      creator: {
+        name: 'dummy data name'
+      },
+      date: new Date()
     }]
   });
 };
