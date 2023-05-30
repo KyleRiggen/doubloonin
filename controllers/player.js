418e92d371c4a6189f00113caf8ede1f753935f9
KyleRiggen <kyle.riggenbach@gmail.com>
2023-05-30 01:44:54 -0600

    got single player model up

--- controllers/player.js
+++ controllers/player.js
@@ -1,23 +1,41 @@
-import Player from "../models/player.js";
+// import Player from "../models/player.js";
+import playerAPI from "../models/player.js";
 
-const getPlayer = (req, res, next) => {
-    Player.fetchAll((p) => {
-        res.render('home/onePlayer', {
-            player: p,
-            pageTitle: 'One Player from the model',
-            path: '/player'
-        });
-    });
-};
-
-// const getMatches = (req, res, next) => {
-//     Match.fetchAll((matches) => {
-//         res.render('home/home', {
-//             mtchs: matches,
-//             pageTitle: 'Home',
-//             path: '/'
+// const getPlayer = (req, res, next) => {
+//     Player.fetchAll((p) => {
+//         res.render('home/onePlayer', {
+//             player: p,
+//             pageTitle: 'One Player from the model',
+//             path: '/player'
 //         });
+//         console.log('inside the controller: ', p);
 //     });
 // };
+
+// const getPlayer = (req, res, next) => {
+//     playerAPI((p) => {
+//         res.render('home/onePlayer', {
+//             player: p,
+//             pageTitle: 'One Player from the model',
+//             path: '/player'
+//         })
+//     }
+//     )
+// }
+
+const getPlayer = (req, res, next) => {
+    playerAPI()
+        .then(data => {
+            res.render('home/onePlayer', {
+                player: data,
+                pageTitle: 'One Player from the model',
+                path: '/player'
+            })
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
 
 export { getPlayer };
--- models/player.js
+++ models/player.js
@@ -1,15 +1,20 @@
-function getPlayerInfo() {
-  return('hello')
+import { RiotAPI, RiotAPITypes, PlatformId } from '@fightmegg/riot-api'
+
+export default async function playerAPI() {
+  // const rAPI = new RiotAPI('RGAPI-ce050746-7ff0-4ee4-84c4-519bc9e3ad5e');
+  const rAPI = new RiotAPI(process.env.RIOT_API);
+
+  try {
+      const summoner = await rAPI.summoner.getBySummonerName({
+        region: PlatformId.NA1,
+        summonerName: "Senna af",
+      });
+      return summoner;
+
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
 }
 
-export default class Player {
-  constructor(n, p, r) {
-    this.name = n;
-    this.points = p;
-    this.region = r;
-  }
-
-  static fetchAll() {
-    getPlayerInfo()
-  }
-}
+export { playerAPI };
